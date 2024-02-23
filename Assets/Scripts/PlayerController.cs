d505d45b61a10ee5d5a4f87194ba334e1e1fe81b
Jo <Jojo@zincke.net>
2024-02-24 00:15:12 +0100

    UnityProject
    add UnityProject

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static string playerName;
+    public static int playerMoney;
+    public static int playerGold;
+    public static long playerXP;
+    public static int playerLevel;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
