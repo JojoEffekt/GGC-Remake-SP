9767135c12a76506fd4ec2879a41ec553a09750a
Jo <Jojo@zincke.net>
2024-03-30 00:42:50 +0100

    Unity: Scripts
    Create DinnerPrefab
    Create DinnerUI

--- Assets/Scripts/Controller/UI/DinnerUIController.cs
+++ Assets/Scripts/Controller/UI/DinnerUIController.cs
@@ -9,12 +9,17 @@ public class DinnerUIController : MonoBehaviour
     public void Start(){
         LoadDinnerItems();
         Debug.Log(DinnerItemList[0].name);
+        Debug.Log(DinnerItemList[2].name);
     }
 
     //läd die gleichbleibenden DinnerItems, danach können die counter der jeweiligen gerichte geladen werden
     public void LoadDinnerItems(){
-        DinnerItemList.Add(new DinnerItem("test","normal","winter",0,new Dictionary<string ,string>(){{"df","dfgg"}},new Dictionary<string ,int>(){{"df",3}},new Dictionary<string ,int>(){{"df",23}},new Dictionary<string ,int>(){{"df",23}}));
-        //CONTINUE
+        //                               name       spriteName    type        Dinnertype level                                                                                                                                                          infoIngredients                                                                                 Superzutat                                stars
+        //DinnerItemList.Add(new DinnerItem("","","","",0,new Dictionary<string ,string>(){{"number",""},{"time",""},{"moneyPerItem",""},{"xp",""},{"moneyPerItemSI",""},{"xpSI",""}},new Dictionary<string ,int>(){{"",}},new Dictionary<string ,int>(){{"",}},new Dictionary<string ,int>(){{"",},{"",},{"",}}));
+        DinnerItemList.Add(new DinnerItem("garden salad","0","type_vegetable","background_dinner",0,new Dictionary<string ,string>(){{"number","10"},{"time","3"},{"moneyPerItem","24"},{"xp","5"},{"moneyPerItemSI","0"},{"xpSI","0"}},new Dictionary<string ,int>(){{"item_02",1},{"item_13",1},{"item_19",1}},new Dictionary<string ,int>(){{"0",0}},new Dictionary<string ,int>(){{"bronze_star_icon",0},{"silver_star_icon",0},{"gold_star_icon",0}}));
+        DinnerItemList.Add(new DinnerItem("tomatosoup","0","type_soup","background_dinner",0,new Dictionary<string ,string>(){{"number","40"},{"time","10"},{"moneyPerItem","14"},{"xp","16"},{"moneyPerItemSI","0"},{"xpSI","0"}},new Dictionary<string ,int>(){{"item_02",1},{"item_13",1},{"item_26",1}},new Dictionary<string ,int>(){{"0",0}},new Dictionary<string ,int>(){{"bronze_star_icon",0},{"silver_star_icon",0},{"gold_star_icon",0}}));
+        DinnerItemList.Add(new DinnerItem("omelette","0","type_burger","background_dinner",0,new Dictionary<string ,string>(){{"number","5"},{"time","1"},{"moneyPerItem","100"},{"xp","2"},{"moneyPerItemSI","0"},{"xpSI","0"}},new Dictionary<string ,int>(){{"item_23",1}},new Dictionary<string ,int>(){{"0",0}},new Dictionary<string ,int>(){{"bronze_star_icon",0},{"silver_star_icon",0},{"gold_star_icon",0}}));
+        DinnerItemList.Add(new DinnerItem("mousse au chocolat","0","type_candy","background_dinner",0,new Dictionary<string ,string>(){{"number","390"},{"time","180"},{"moneyPerItem","4"},{"xp","259"},{"moneyPerItemSI","0"},{"xpSI","0"}},new Dictionary<string ,int>(){{"item_23",1},{"item_29",1},{"item_26",1}},new Dictionary<string ,int>(){{"0",0}},new Dictionary<string ,int>(){{"bronze_star_icon",0},{"silver_star_icon",0},{"gold_star_icon",0}}));
     }
 }
 
@@ -22,6 +27,7 @@ public class DinnerUIController : MonoBehaviour
 public class DinnerItem {
 
     public string name { get; set; }                                //Gerichte Bezeichnung
+    public string spriteName { get; set; }                          //gerichte sprite name
     public string typeDinner { get; set; }                          //Main,Vegetable,Burger,Soup,Fruit,Candy   Einordung des Gerichts
     public string type { get; set; }                                //normal,winter,gold                       Zuordnung des Gerichts
     public int counter { get; set; }                                //(gekochte male als zahl)
@@ -32,8 +38,9 @@ public class DinnerItem {
     public Dictionary<string, int> star { get; set; }               //(bronze ab:...,silber..,...)
 
     //instanziiert das object
-    public DinnerItem(string name, string typeDinner, string type, int level, Dictionary<string, string> info, Dictionary<string, int> infoIngredients, Dictionary<string, int> specialIngredient, Dictionary<string, int> star){                    
+    public DinnerItem(string name, string spriteName, string typeDinner, string type, int level, Dictionary<string, string> info, Dictionary<string, int> infoIngredients, Dictionary<string, int> specialIngredient, Dictionary<string, int> star){                    
         this.name = name;
+        this.spriteName = spriteName;
         this.typeDinner = typeDinner;
         this.type = type;
         this.level = level;
