80ee739ecb2a5ccf14ae1dd1905056c603582fac
Jo <Jojo@zincke.net>
2024-04-26 17:51:32 +0200

    Unity: Script
    update player char with new colors

--- Assets/Scripts/Controller/Builder/PlayerCharBuilder.cs
+++ Assets/Scripts/Controller/Builder/PlayerCharBuilder.cs
@@ -20,6 +20,8 @@ public class PlayerCharBuilder : MonoBehaviour
 
         //läd daten als referenz
         player = new Player(bool.Parse(PlayerDict["Gender"]),PlayerDict["Hat"],PlayerDict["Face"],PlayerDict["Hair"],PlayerDict["HairOverlay"],PlayerDict["Leg"],PlayerDict["LegOverlay"],PlayerDict["Skin"],PlayerDict["SkinOverlay"],PlayerDict["Tshirt"],PlayerDict["TshirtOverlay"]);
+
+        player.setFace(0.1f,1f,0.5f,1);
     }
 
     public static void LoadSprites(){
@@ -35,7 +37,7 @@ public class Player
 {
     public bool gender { get; set; }                //true = male, false = female
     public string hat { get; set; }                 //farbe für das jeweilige sprite
-    public string face { get; set; }                //farbe für das jeweilige sprite
+    public float[] face { get; set; }                //farbe für das jeweilige sprite
     public string hair { get; set; }                //farbe für das jeweilige sprite
     public string hair_overlay { get; set; }        //farbe für das jeweilige sprite
     public string leg { get; set; }                 //farbe für das jeweilige sprite
@@ -48,7 +50,7 @@ public class Player
     public Player(bool gender, string hat, string face, string hair, string hair_overlay, string leg, string leg_overlay, string skin, string skin_overlay, string tshirt, string tshirt_overlay){
         this.gender = gender;
         this.hat = hat;
-        this.face = face;
+        this.face = new float[]{float.Parse(face.Split("-")[0]),float.Parse(face.Split("-")[1]),float.Parse(face.Split("-")[2]),float.Parse(face.Split("-")[3])};
         this.hair = hair;
         this.hair_overlay = hair_overlay;
         this.leg = leg;
@@ -57,5 +59,17 @@ public class Player
         this.skin_overlay = skin_overlay;
         this.tshirt = tshirt;
         this.tshirt_overlay = tshirt_overlay;
+
+        Debug.Log("face: "+this.face[0]);
     }
+
+    //setzte neuen farbwert für das gesicht
+    public void setFace(float r, float g, float b, float a){
+        Debug.Log("val: "+r);
+    }
+
+    //CONITNUE
+    //...
+    //update player char
+    // mache hier funktionen hin um palyer farbe zu ändern (über shop)
 }
