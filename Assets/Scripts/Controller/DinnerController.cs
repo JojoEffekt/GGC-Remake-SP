ffc019a042b2a9fbc58f52217b8f3806be8b009d
Jo <Jojo@zincke.net>
2024-05-05 23:27:14 +0200

    Unity: Scripts
    start with cooking dinner

---
+++ Assets/Scripts/Controller/DinnerController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinnerController : MonoBehaviour
+{
+    //managed die gerichte die gerade auf den öfen gekocht werden
+}
